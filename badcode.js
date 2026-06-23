80aef173c6690d69657a4236a59a98a097a1650d
E.MANOJ UARDHAN REDDY <manojvardhanreddy2004@gmail.com>
2026-06-23 14:31:30 +0530

    Create badcode.js

---
+++ badcode.js
@@ -0,0 +1,16 @@
+var password = "admin123";
+
+function login() {
+    var unused = 10;
+    console.log(password);
+}
+
+function login2() {
+    var unused = 10;
+    console.log(password);
+}
+
+function login3() {
+    var unused = 10;
+    console.log(password);
+}
